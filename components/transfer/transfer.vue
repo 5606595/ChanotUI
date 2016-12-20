db3e40e3a1ef15306284a5d51627860a16cc59b2
5606595 <475365254@qq.com>
2016-12-20 16:17:21 +0800

    transfer init

--- components/transfer/transfer.vue
+++ components/transfer/transfer.vue
@@ -1,7 +1,11 @@
 <template>
     <div class="c-transfer">
         <div class="transfer-from">
-
+            <ul v-for="source in sourcedata">
+                <li :title="source.title" :disabled="source.disabled">
+                    {{ source.content }}
+                </li>
+            </ul>
         </div>
         <div class="transfer-symbol">
 
@@ -20,6 +24,24 @@
             return {
 
             }
-        }
+        },
+        props: {
+            sourcedata: {
+                type: Array,
+                default: [{
+                    content: 'task1',
+                    title: 'task1',
+                    disabled: false
+                }]
+            },
+            targetdata: {
+                type: Array,
+                default: null
+            },
+            title: {
+                type: Array,
+                default: ['source', 'target']
+            }
+        },
     })
 </script>
