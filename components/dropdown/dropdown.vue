1a3eceb7a307203178a93539445d08f969f689a0
5606595 <475365254@qq.com>
2017-02-05 15:58:13 +0800

    dropdown

---
+++ components/dropdown/dropdown.vue
@@ -0,0 +1,18 @@
+<template>
+
+</template>
+<style scoped rel="stylesheet/less" lang="less">
+
+</style>
+<script type="text/ecmascript-6">
+    export default {
+        data() {
+            return {
+
+            }
+        },
+        components: {
+
+        }
+    }
+</script>
---
+++ components/dropdown/index.js
@@ -0,0 +1,6 @@
+/**
+ * Created by jorten on 2017/2/3.
+ */
+import dropdown from 'dropdown.vue'
+
+export default dropdown
